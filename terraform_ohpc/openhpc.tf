efc1e7ee591fe5c9ab8c8ad4b12b8682a761a5f5
Steve Brasier <steveb@stackhpc.com>
2020-01-21 16:43:56 +0000

    output public IP for ohpc-login

--- terraform_ohpc/openhpc.tf
+++ terraform_ohpc/openhpc.tf
@@ -86,3 +86,8 @@ resource "local_file" "hosts" {
   filename = "ohpc_hosts"
   depends_on = [openstack_compute_instance_v2.compute]
 }
+
+output "ophc_login_public_ip" {
+  value = openstack_networking_floatingip_v2.fip_1.address
+  description = "Public IP for OpenHPC login node"
+}
